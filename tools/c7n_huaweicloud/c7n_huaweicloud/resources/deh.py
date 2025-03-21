0fab6af0aebb5bc2114b002c46ebe0d8042ad945
liyifan <liyifan56@huawei.com>
2025-03-21 15:43:51 +0800

    add update-dedicated-host action

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/deh.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/deh.py
@@ -22,6 +22,7 @@ class Deh(QueryResourceManager):
         id = 'dedicated_host_id'
         tag = True
 
+
 @Deh.action_registry.register("update-dedicated-host")
 class UpdateDedicatedHost(HuaweiCloudBaseAction):
     """Update Dedicated Host.
@@ -33,26 +34,27 @@ class UpdateDedicatedHost(HuaweiCloudBaseAction):
         policies:
           - name: update-dedicated-host
             resource: huaweicloud.deh
-            flters:
+            filters:
               - type: value
-                key: metadata.__system__encrypted
-                value: "0"
+                key: name
+                value: "test"
             actions:
-              - delete
+              - type: update-dedicated-host
+                dedicated_host:
+                    name: "update"
+                    auto_placement: "off"
     """
 
     schema = type_schema(
         "update-dedicated-host",
-        required=['dedicated_host_id'],
+        required=['dedicated_host'],
         **{
-            "dedicated_host_id": {"type": "string"},
             "dedicated_host": {
                 "type": "object",
                 "properties": {
                     "auto_placement": {"type": "string", "enum": ["on", "off"]},
                     "name": {"type": "string"},
                 }
-
             },
         }
     )
@@ -60,8 +62,14 @@ class UpdateDedicatedHost(HuaweiCloudBaseAction):
     def perform_action(self, resource):
         client = self.manager.get_client()
         request = UpdateDedicatedHostRequest()
+        request.dedicated_host_id = resource['dedicated_host_id']
+        dedicated_host = self.data.get("dedicated_host", None)
+        if dedicated_host:
+            request.body = ReqUpdateDeh(
+                dedicated_host=ReqUpdateDehMessage(
+                    auto_placement=dedicated_host.get("auto_placement", None),
+                    name=dedicated_host.get("name", None),
+                )
+            )
         response = client.update_dedicated_host(request)
-        log.info(f"Received Job ID:{response.job_id}")
-        # TODO: need to track whether the job succeed
-        response = None
         return response
